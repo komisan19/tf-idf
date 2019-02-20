83c5ab2df48804e98ddaa7ca95d09696b35ddc73
komisan19 <komiyama6219@gmail.com>
2019-02-21 03:10:35 +0900

    Add negative and positive

--- read_csv.rb
+++ read_csv.rb
@@ -1,7 +1,17 @@
 require 'csv'
 
 # csvファイルを読み込み
-def read
-  texts = CSV.read("****", encoding: "Shift_JIS:UTF-8")
+def read_all
+  texts = CSV.read("csv/note_all.csv", encoding: "Shift_JIS:UTF-8")
+end
+
+# 多分いらないと思う
+
+def read_positive
+  texts = CSV.read("csv/note_positive.csv", encoding: "Shift_JIS:UTF-8")
+end 
+
+def read_negative
+  texts = CSV.read("csv/note_negative.csv", encoding: "Shift_JIS:UTF-8")
 end
 
